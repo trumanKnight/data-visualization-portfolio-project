a5c9c86b177e94f961e17778f63f249c477daf49
trumanKnight <cvilletruman@gmail.com>
2021-11-17 12:59:35 -0500

    Update script.py

--- scripts/script.py
+++ scripts/script.py
@@ -14,7 +14,7 @@ iPATH = PATH + '/images/'
 df = pd.read_csv(dPATH + 'all_data.csv')
 
 GDP_descending = df.groupby('Country', as_index=False)[['Country','GDP']].mean()
-GDP_average_descending = GDP_descending.sort_values('GDP', ascending = Fals, ignore_index  = True)
+GDP_average_descending = GDP_descending.sort_values('GDP', ascending = False, ignore_index  = True)
 
 time_span = df.Year.max() - df.Year.min() + 1
 
@@ -27,9 +27,9 @@ plt.savefig(iPATH + f'Trailing {time_span} Year Average GDP by Country.PNG')
 plt.clf()
 
 fig = plt.figure(figsize = (12,12))
-ax = sns.linplot(data = df, x = 'Year', y = 'GDP', hue = 'Country')
+ax = sns.lineplot(data = df, x = 'Year', y = 'GDP', hue = 'Country')
 
-plt.savefig(iPATH + 'GDP vs. Time | hue = Country.PNG')
+plt.savefig(iPATH + 'GDP vs. Time -- hue = Country.PNG')
 plt.clf()
 
 countries = list(df['Country'].unique())
@@ -37,24 +37,24 @@ countries = list(df['Country'].unique())
 i = 1
 fig = plt.figure(figsize = (20,20))
 for country in countries:
-	df_temp = df[df.Country == countries]
+	df_temp = df[df.Country == country]
 	ax = fig.add_subplot(2,3,i)
 	sns.regplot(data = df_temp, x = 'Life expectancy at birth (years)', y = 'GDP')
 	plt.title(f'{country} Life Expectancy vs. GDP')
 	i += 1
 
-plt.savefig(iPATH + 'Life Expectancy vs. GDP | 2x3.PNG')
+plt.savefig(iPATH + 'Life Expectancy vs. GDP -- 2x3.PNG')
 plt.clf()
 
 total_GDP_by_year = df.groupby('Year').sum()['GDP']
 
-for row in df.index():
+for row in df.index:
 	index = df.iloc[row]['Year']
 	df.loc[row, 'Total GDP'] = total_GDP_by_year[index]
 df['Pct of GDP'] = df['GDP'] / df['Total GDP']*100
 
 i = 1
-fig = plt.figure(figsize(20,20))
+fig = plt.figure(figsize = (20,20))
 for country in countries:
 	df_temp = df[df.Country == country]
 
@@ -63,7 +63,7 @@ for country in countries:
 	ax.legend(loc = 'upper left')
 
 	ax2 = ax.twinx()
-	sns.regplot(data = df_temp, x = 'Year', y = 'Life expectancy at birth (years)', label = 'Life Expectancy')
+	sns.regplot(data = df_temp, x = 'Year', y = 'Life expectancy at birth (years)', label = 'Life Expectancy', color = 'red')
 	ax2.legend(loc = 'upper right')
 
 	plt.title(f'{country} Life Expectancy\nvs. GDP')
